13fab44211d259f7ff649f808237dfdfdd928ede
Doojin Kang <doojin.kang@gmail.com>
2017-09-02 01:50:34 +0900

    Home(/) redirect to /created

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='container'>
-      Home will redirect to '/created or /trending'
-    </div>
+    <Redirect to='/created'/>
   );
 };
 
