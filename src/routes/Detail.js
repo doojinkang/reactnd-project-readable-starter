27ba0579986aa46c05466782e5a0736770a92edc
Doojin Kang <doojin.kang@gmail.com>
2017-09-05 06:19:16 +0900

    Detail View use store and props.match.params.id

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Detail extends Component {
   render() {
-    const { id } = this.props.match.params
+    const post = this.props.posts[this.props.match.params.id]
+    console.log('...', post)
     return (
       <div className='container'>
-        Detail {id}
+
+        <div className='form-group'>
+          <span className = 'label label-default'> Title </span>
+          <div>
+            {post.title}
+          </div>
+        </div>
+        <div className='form-group'>
+          <span className = 'label label-default'> Body </span>
+          <div>
+            {post.body}
+          </div>
+        </div>
+        <div className='form-group'>
+          <span className = 'label label-default'> Author </span>
+          <div>
+            {post.author}
+          </div>
+        </div>
+        <div className='form-group'>
+          <span className = 'label label-default'> Tag </span>
+          <div>
+            {post.category}
+          </div>
+        </div>
+        <div style={{marginTop:'10px'}}>
+          <button className='btn btn-default'>vote</button>
+        </div>
+
       </div>
     )
   }
 }
 
-export default Detail
+
+function mapStateToProps(posts) {
+  return {
+    posts: posts
+  }
+}
+
+export default connect(mapStateToProps, undefined)(Detail)
