d95390c4416746948cacdcefd754f9e8ee93f707
Doojin Kang <doojin.kang@gmail.com>
2017-09-05 20:31:44 +0900

    Detail View shows comments from PostAPI

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,12 +4,13 @@ import * as PostAPI from '../PostAPI'
 
 class Detail extends Component {
   state = {
-    detail : {
+    post : {
       title: '',
       body: '',
       author: '',
       category: '',
-    }
+    },
+    comments: []
   }
 
   componentDidMount() {
@@ -17,13 +18,22 @@ class Detail extends Component {
       console.log('API.getPostDetail', data)
       this.setState( {
         ...this.state,
-        detail: data
+        post: data
+      })
+    })
+
+    PostAPI.getComments(this.props.match.params.id).then( (data) => {
+      console.log('API.getComments', data)
+      this.setState( {
+        ...this.state,
+        comments: data
       })
     })
   }
 
   render() {
-    const post = this.state.detail
+    const post = this.state.post
+    const comments = this.state.comments
     console.log('...', post)
     return (
       <div className='container'>
@@ -54,6 +64,43 @@ class Detail extends Component {
         </div>
         <div style={{marginTop:'10px'}}>
           <button className='btn btn-default'>vote</button>
+        </div>
+
+        <hr/>
+        <div>
+          <table className='table table-bordered table-hover'>
+            <thead>
+              <tr>
+                <th>Comment</th>
+                <th>Author</th>
+                <th>Date</th>
+                <th>vote</th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>
+
+          {comments.map((comment)=>(
+            <tr>
+              <td>
+                { comment.body }
+              </td>
+              <td>
+                { comment.author }
+              </td>
+              <td>
+                { comment.timestamp }
+              </td>
+              <td>
+                { comment.voteScore }
+              </td>
+              <td>
+                <button className='btn btn-default'>vote</button>
+              </td>
+            </tr>
+          ))}
+            </tbody>
+          </table>         
         </div>
 
       </div>
